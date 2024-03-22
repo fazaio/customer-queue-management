8815077d7929327525fe3e53e497a4969e1c6260
fazaio <faza6028@gmail.com>
2024-03-23 05:06:20 +0700

    done all

--- testing-app/src/app/visitor-list/visitor-list.component.ts
+++ testing-app/src/app/visitor-list/visitor-list.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 export class VisitorListComponent implements OnInit {
 
   constructor(private elementRef: ElementRef, private http: HttpClient, private router: Router) { }
-  dataSource: PeriodicElement[] = [];
+  dataSource: VisitorInterface[] = [];
 
   ngOnInit(): void {
     this.fetchData()
@@ -27,30 +27,28 @@ export class VisitorListComponent implements OnInit {
         console.log(data);
       });
   }
-
   changePage(path) {
     this.router.navigate(['/' + path]);
   }
 
-  displayedColumns: string[] = ['full_name', 'address', 'phone_number', 'queue_number'];
-  // dataSource = ELEMENT_DATA;
+  applyFilter(value: any) {
+    console.log(this.dataSource);
+    if(value.length > 0){
+      const filteredArray = this.dataSource.filter(item => {
+        // Convert the full_name to lowercase and check if it includes the search string
+        return item.full_name.toLowerCase().includes(value.toLowerCase());
+      });
+      this.dataSource = filteredArray
+    }else{
+      this.fetchData()
+    }
+  }
 
+  displayedColumns: string[] = ['full_name', 'address', 'phone_number', 'queue_number'];
 }
 
-// const ELEMENT_DATA: PeriodicElement[] = [
-//   { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-//   { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-//   { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-//   { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-//   { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-//   { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-//   { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-//   { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-//   { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-//   { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-// ];
 
-export interface PeriodicElement {
+export interface VisitorInterface {
   full_name: string;
   address: string;
   phone_number: string;
